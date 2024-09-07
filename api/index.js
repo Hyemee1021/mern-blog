b1982531c87e33e7067e9974cbd08c52a062cf5f
Hye Mee Park <hyemee1021@gmail.com>
2024-09-07 17:57:55 +1000

    created database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,15 @@
 import express from "express";
+import mongoose, { mongo } from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
+mongoose
+  .connect(process.env.MONGODB)
+  .then(() => console.log("MongoDB is connected"))
+  .catch((error) => console.log(error.mesage));
 const app = express();
 
 app.listen(3000, () => {
-  console.log("Server is running on port 300!!");
+  console.log("Server is running on port 3000!!");
 });
+//1:01:19
